668a6cbb6f9d3af3c8ddf7d1429b21d96406ffb0
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-13 22:36:32 +0200

    v2

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -6,10 +6,14 @@
         {
             #region vedio 1
 
-            rect rect = new rect();
-            rect.Dim1 = 4;
-            rect.Dim2 = 5;
-            Console.WriteLine( rect.calcArea());
+            //rect rect = new rect();
+            //rect.Dim1 = 4;
+            //rect.Dim2 = 5;
+            //Console.WriteLine( rect.calcArea());
+            #endregion
+
+            #region vedio 2
+            Console.WriteLine(Utility.CalcArea(8));
             #endregion
         }
     }
---
+++ Demo/Utility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    //static class
+    //containar of static member [property,constructor ,method]
+   // and constant
+   //you cant create object from this class (helper class)
+   //no inheritance from this class
+
+    internal static class Utility
+    {
+       
+
+        //class member property must deal with [static attribute or constant =>with only static]
+
+        //static property 
+        //readonly with static or non static
+        //private static readonly double pi = 3.14;
+
+
+        //Constant 
+        private const double pi = 3.14;
+        //static constructor omly one per class
+        //you cant specify acces modifier or paramter
+        //will be called just one time per class lifetime befor first usage of the class
+        static Utility()
+        {
+            //pi = 3.14;
+        }
+
+        public static double  CalcArea (double radius)
+        {
+            return Utility.pi * radius * radius;
+        }
+    }
+}
