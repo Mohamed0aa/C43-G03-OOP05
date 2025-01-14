fb630a4b4e300cc875d48b8c5b01256ac0d3d571
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-14 05:57:33 +0200

    project 1

---
+++ assignment/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region project 1
+            ////1
+            //_3DPoint p =new _3DPoint(0,0,0);
+
+            //2
+            //Console.WriteLine(p);
+
+
+            ////3
+            //p.X=double.Parse(Console.ReadLine());
+            //p.Y=double.Parse(Console.ReadLine());
+            //p.Z = double.Parse(Console.ReadLine());
+
+            ////4
+            //_3DPoint p = new _3DPoint(0, 0, 0);
+            //_3DPoint p1 = new _3DPoint(0, 0, 0);
+
+            //if (p == p1)
+            //{
+            //    Console.WriteLine("p1=p2");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("p1!=p2");
+            //}
+
+            ////5
+            //_3DPoint[] POINTS = new _3DPoint[3];
+            //POINTS[0] = new _3DPoint(1, 2, 3);
+            //POINTS[1] = new _3DPoint(2, 3, 3);
+            //POINTS[2] = new _3DPoint(0, 0, 0);
+
+            //Array.Sort(POINTS);
+
+            //for (int i = 0; i < POINTS.Length; i++)
+            //{
+            //    Console.WriteLine(POINTS[i]);
+            //}
+
+            //_3DPoint p = new _3DPoint(1, 2, 3);
+            //_3DPoint p2 = p;
+
+
+
+
+            #endregion
+        }
+    }
+}
